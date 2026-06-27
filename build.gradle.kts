a6d6d43e5dd23cc50789d28213d76f9e546504e8
Bunyod <podshoevbunyod16@gmail.com>
2026-06-27 10:29:33 +0500

    fix: исправлен Kotlin 2.1.21 и AGP 8.9.2 (стабильные версии)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file
 plugins {
-    id("com.android.application") version "8.10.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
+    id("com.android.application") version "8.9.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.21" apply false
 }
